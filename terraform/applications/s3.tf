53999ab87a7ae11dcbd2461a16e0b63a4903db00
Juraj Seffer <juraj.seffer@travelex.com>
2019-06-20 10:54:57 +0000

    Fixed org

--- terraform/applications/s3.tf
+++ terraform/applications/s3.tf
@@ -1,23 +1,23 @@
 resource "aws_s3_bucket" "middleware" {
-  bucket = "${organisation}-${local.name_prefix}-middleware"
+  bucket = "${var.organisation}-${local.name_prefix}-middleware"
   acl    = "private"
 
   tags = "${merge(
     local.common_tags,
     map(
-      "Name", "${organisation}-${local.name_prefix}-middleware",
+      "Name", "${var.organisation}-${local.name_prefix}-middleware",
     )
   )}"
 }
 
 resource "aws_s3_bucket" "lambda_source" {
-  bucket = "${organisation}-${local.name_prefix}-lambda-source"
+  bucket = "${var.organisation}-${local.name_prefix}-lambda-source"
   acl    = "private"
 
   tags = "${merge(
     local.common_tags,
     map(
-      "Name", "${organisation}-${local.name_prefix}-lambda-source",
+      "Name", "${var.organisation}-${local.name_prefix}-lambda-source",
     )
   )}"
 }
